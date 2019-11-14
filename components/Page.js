27defca9cac13ecad0e5bc81dbf613d8c3a508ee
Scott Agirs <scott@ijs.to>
2019-11-14 12:18:25 -0500

    [add temp] default layout

--- components/Page.js
+++ components/Page.js
@@ -3,8 +3,8 @@ import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Meta from "./Meta";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
-import { black } from "../utils/Colors";
 import theme from "../lib/theme";
+import DefaultLayout from "./layout/DefaultLayout";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -46,6 +46,14 @@ const GlobalStyle = createGlobalStyle`
     padding: 0 3em 2em;
   }
 
+  .input-row {
+    div {
+      display: flex;
+      flex-direction: column;
+    }
+    margin-bottom: 0.75em;
+  }
+
   h1 {
     font-size: 3.5em;
     letter-spacing: -3px;
@@ -82,9 +90,41 @@ const GlobalStyle = createGlobalStyle`
   h6 {
     text-shadow: 0px -2px 0 #fff, 0px -0px 0 #fff, -1px 0px 2 #fff,
       0px 1px 0 #fff;
-
   }
 
+  label {
+    margin: 0.7em;
+    font-size: 0.75em;
+  }
+  
+  input, textarea {
+    background: white;
+    border: 0;
+    border-color: rgb(209, 218, 225);
+    border-radius: 2px;
+    border-style: solid;
+    border-width: 2px;
+    box-sizing: border-box;
+    color: rgb(0, 0, 0);
+    font-family: 'Comfortaa';
+    font-size: 20px;
+    -webkit-appearance: none;
+    line-height: 1.3;
+    margin: 0px;
+    max-width: 100%;
+    outline: none;
+    padding: 16px 20px;
+    width: 100%;
+    &:hover {
+      border-color: rgb(0, 0, 0);
+    }
+    &:focus {
+      background-color: rgb(255, 255, 255);
+      color: rgb(64, 126, 241);
+      box-shadow: rgb(187, 187, 187) 0px 0px 5px 2px;
+      border-color: rgb(64, 126, 241);
+    }
+  }
 `;
 
 const Page = ({ children }) => {
@@ -92,8 +132,7 @@ const Page = ({ children }) => {
     <ThemeProvider theme={theme}>
       <Meta />
       <Header />
-
-      {children}
+      <DefaultLayout>{children}</DefaultLayout>
       <Footer />
       <GlobalStyle />
     </ThemeProvider>
