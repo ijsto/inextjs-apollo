82ff6c7ed70bb36215cc0110d7f54d4b849ba5f5
Scott Agirs <scott@ijs.to>
2019-11-14 12:19:32 -0500

    [Add] PostGrid Styles

--- components/CourseQuery.js
+++ components/CourseQuery.js
@@ -2,6 +2,34 @@ import { useQuery } from "react-apollo";
 import FIRST_QUERY from "../api/query/FIRST_QUERY";
 import CourseDeleteOne from "./CourseDeleteOne";
 
+import styled from "styled-components";
+
+const PostGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1em;
+  min-width: 300px;
+  max-width: 1584px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2em;
+  box-sizing: border-box;
+  .entry {
+    background: white;
+    border-radius: 0.75em;
+    box-shadow: rgb(216, 216, 216) 0px 3px 2px -2px;
+    margin: 0 1%;
+    padding: 0.5em;
+    h3 {
+      font-size: 18px;
+      margin: 2px 0 10px 0;
+    }
+    p {
+      font-size: 15px;
+    }
+  }
+`;
+
 export const CourseQuery = () => {
   const { data, loading, error } = useQuery(FIRST_QUERY);
   if (loading) return "Loading..";
@@ -9,20 +37,26 @@ export const CourseQuery = () => {
   console.log({ data });
 
   return (
-    <div>
-      <h2>Query</h2>
-      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr" }}>
-        {data.courses.map(c => (
-          <div key={c.id}>
-            <div style={{ display: "grid", alignItems: "baseline" }}>
-              <h3>{c.title}</h3>
-              <CourseDeleteOne courseId={c.id}></CourseDeleteOne>
-            </div>
-            <p>{c.summary}</p>
+    <PostGrid>
+      {data.courses.map(c => (
+        <div>
+          <div className="entry" key={c.id}>
+            <h3>{c.title}</h3>
+
+            <p style={{ padding: "1em 0" }}>{c.summary}</p>
           </div>
-        ))}
-      </div>
-    </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              margin: "0.5em"
+            }}
+          >
+            <CourseDeleteOne courseId={c.id}></CourseDeleteOne>
+          </div>
+        </div>
+      ))}
+    </PostGrid>
   );
 };
 
